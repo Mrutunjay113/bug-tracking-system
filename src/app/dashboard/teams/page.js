16e31b2ccffab46e3db1f7f0bab743f92835a6d2
Mrutunjay Yadav <42071203+Mrutunjay113@users.noreply.github.com>
2024-08-12 14:57:21 +0530

    Refactor Page component to use lazy loading for TeamTable component

--- src/app/dashboard/teams/page.js
+++ src/app/dashboard/teams/page.js
@@ -42,8 +42,9 @@ export default async function Page({ searchParams }) {
           </Link>
         </div>
       </div>
+      {userss ? <TeamTable data={userss} /> : <p>No data available</p>}
       {/* <Suspense fallback={<div>Loading...</div>}>
-        // {userss ? <TeamTable data={userss} /> : <p>No data available</p>}
+      
       </Suspense> */}
     </main>
   );
